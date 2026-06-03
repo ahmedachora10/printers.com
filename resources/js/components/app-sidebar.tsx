34e588265e0677f8ce4c9df9d9b6701ca1e35348
ahmedachora10 <gjr8195@gmail.com>
2026-06-03 16:10:02 +0100

    Remove unused NavFooter import from AppSidebar component

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,10 +1,9 @@
-import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { ArrowLeftRight, BookOpen, FileText, Folder, GitBranch, LayoutGrid, LucideIcon, Package, ServerIcon, ShoppingCart, Users, Wallet } from 'lucide-react';
+import { ArrowLeftRight, FileText, GitBranch, LayoutGrid, LucideIcon, Package, ServerIcon, ShoppingCart, Users, Wallet } from 'lucide-react';
 import AppLogo from './app-logo';
 
 
